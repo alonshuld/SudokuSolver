2876dd0db5e0e3f62cb25ca5e41f9cb70040232b
alonsd <alonshuld@gmail.com>
2022-12-07 15:12:20 +0200

    Added function that checks if a file is valid to be a puzzle and function to load a puzzle into a list

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+CHAR_SPLIT = ' '
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_puzzle_file(file_name):
+    """
+    This function checks if a file is valid to be used as a puzzle
+    :param file_name: string that contains the file name
+    :return: bool answer
+    """
+    with open(file_name, 'r') as file:
+        if len(file.readlines()) != 9:
+            return False
+        file.seek(0)
+        for line in file.readlines():
+            line = line.strip()
+            if len(line) != 17:
+                return False
+            for i in range(len(line)):
+                if i % 2 == 0:
+                    if line[i] < '0' or line[i] > '9':
+                        return False
+                else:
+                    if line[i] != CHAR_SPLIT:
+                        return False
+    return True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def load_puzzle(file_name):
+    """
+    This function loads a puzzle from txt file
+    :param file_name: a string that contains the file name
+    :return: returns a list
+    """
+    if not check_puzzle_file(file_name):
+        print("The file is corrupted")
+        exit()
+    board = []
+    with open(file_name, 'r') as file:
+        for line in file.readlines():
+            board.append((line.strip()).split(CHAR_SPLIT))
+    return board
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    file_name = 'puzzle.txt'
+    print(load_puzzle(file_name))
+
+
+if __name__ == "__main__":
+    main()
